486f90a72c3359cd601279cfc230f276dc0aeca3
sxdny <67187745+sxdny@users.noreply.github.com>
2024-09-23 09:43:47 +0200

    added dotfiles updated
    now located on my .config folder

---
+++ nvim/lua/config/autocmds.lua
@@ -0,0 +1,11 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+--
+-- Disable Spell Checking (Markdowm & txt files)
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "text" },
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
